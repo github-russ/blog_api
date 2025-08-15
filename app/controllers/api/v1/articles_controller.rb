c726cc775bbc9c63b609b4246108f4322dbc9146
russ <cacdacdave@yahoo.com>
2025-08-15 19:20:16 +0800

    Commit of blog API project

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,47 @@
+class Api::V1::ArticlesController < ApplicationController
+  before_action :set_article, except: [:index, :create]
+
+  def index
+    @articles = Article.all
+
+    render json: { message: "Success", data: @articles, count: @articles.count }
+  end
+
+  def show
+    render json: @article, include: :comments
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      render json: @article, status: :created, location: api_v1_articles_url(@article)
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      render json: @article
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article.destroy
+
+    render json: { message: "Successfully deleted an article API!"}
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,52 @@
+class Api::V1::CommentsController < ApplicationController
+  before_action :set_article
+  before_action :set_comment, only: [:show, :update, :destroy]
+
+  def index
+    @comments = @article.comments
+
+    render json: { message: "Success", data: @comments, count: @comments.count }
+  end
+
+  def show
+    render json: @comment
+  end
+
+  def create
+    @comment = @article.comments.new(comment_params)
+
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @comment.update(comment_params)
+      render json: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @comment.destroy
+
+    render json: { message: "Successfully deleted comment API!" }
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:article_id])
+  end
+
+  def set_comment
+    @comment = @article.comments.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
